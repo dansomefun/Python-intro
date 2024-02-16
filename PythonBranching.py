63562075a2be66369610fd40a118fdec1d843db1
dansomefun <dansomefun@yahoo.com>
2024-02-16 10:49:14 -0600

    This is Python branching

---
+++ PythonBranching.py
@@ -0,0 +1,31 @@
+# Making decisions, evaluate decisions  
+# if, elif and else 
+
+# if  
+# if condition:                True
+#     statement1 
+#     statement2
+ 
+    #  TRUE 
+if 90 > 100:
+    print("90 is less than 100")
+    print(90 + 100)
+else: 
+    print("90 is NOT less than 100")
+
+
+# if, elif, else 
+day = "Friday"
+
+
+if day == "Monday": 
+    print("Today is Monday")
+elif day == "Friday": 
+    print("Today is 1st Friday")
+elif day == "Wednesday": 
+    print("Today is Wednesday")
+elif day == "Friday": 
+    print("Today is Friday")
+else: 
+    print("Today is Sunday")
+
